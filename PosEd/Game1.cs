d79767b8f35184c3f5545878f9dcca064cec9920
Georges Benatti Jr <gbenatti@gmail.com>
2014-04-07 13:38:51 -0300

    Extract Level and Bathysphere Entities

--- PosEd/Game1.cs
+++ PosEd/Game1.cs
@@ -33,30 +33,222 @@ namespace PosEd
 	/// </summary>
 	public class Game1 : Game
 	{
+		class LevelEntity
+		{
+			Texture2D wallsTB;
+			Texture2D wallsRL;
+			Texture2D wallsT;
+			Texture2D wallsB;
+			Texture2D wallsR;
+			Texture2D wallsL;
+			Texture2D noWalls;
+			Texture2D wallsBL;
+			Texture2D wallsTL;
+			Texture2D wallsBR;
+			Texture2D wallsTR;
+			Texture2D wallsLTB;
+			Texture2D wallsRTB;
+			Texture2D wallsLTRB;
+
+			Texture2D wallsLBR;
+
+			Texture2D wallsLTR;
+
+			int mapWidth;
+
+			int mapHeight;
+
+			LevelDescription level;
+
+			public LevelEntity (LevelDescription level, int mapWidth, int mapHeight)
+			{
+				this.level = level;
+				this.mapWidth = mapWidth;
+				this.mapHeight = mapHeight;
+			}
+
+			public void Load(ContentManager content)
+			{
+				wallsTB = content.Load<Texture2D> ("square-tb");
+				wallsRL = content.Load<Texture2D> ("square-rl");
+				wallsT = content.Load<Texture2D> ("square-t");
+				wallsB = content.Load<Texture2D> ("square-b");
+				wallsR = content.Load<Texture2D> ("square-r");
+				wallsL = content.Load<Texture2D> ("square-l");
+
+				noWalls = content.Load<Texture2D> ("square");
+
+				wallsBL = content.Load<Texture2D> ("square-bl");
+				wallsTL = content.Load<Texture2D> ("square-lt");
+				wallsBR = content.Load<Texture2D> ("square-rb");
+				wallsTR = content.Load<Texture2D> ("square-tr");
+
+				wallsLTB = content.Load<Texture2D> ("square-ltb");
+				wallsRTB = content.Load<Texture2D> ("square-rtb");
+				wallsLTRB = content.Load<Texture2D> ("square-ltrb");
+
+				wallsLBR = content.Load<Texture2D> ("square-lbr");
+				wallsLTR = content.Load<Texture2D> ("square-ltr");
+			}
+
+			public void Render(LevelDescription level, SpriteBatch spriteBatch)
+			{
+				for (int l = 0; l < level.Height; l++) {
+					for (int c = 0; c < level.Width; c++) {
+						var rectangle = new Rectangle (c * TILE_WIDTH, l * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+						var texture = SelectRoomTexture (l, c);
+						var color = SelectRoomColor (l, c);
+						if (!IsEmptyRoom (l, c)) {
+							if (texture != null) {
+								spriteBatch.Draw (texture, rectangle, null, color);
+							}
+							else {
+								DumpTileInfo (l, c);
+							}
+						}
+					}
+				}
+			}
+
+			Texture2D SelectRoomTexture (int l, int c)
+			{
+				Texture2D texture = null;
+				var targetTileType = level.Tiles [c + l * mapWidth];
+
+				if ((targetTileType.Walls ^ (WallTypes.Top|WallTypes.Bottom)) == 0) {
+					texture = wallsTB;
+				}
+				if ((targetTileType.Walls ^ (WallTypes.Right|WallTypes.Left)) == 0) {
+					texture = wallsRL;
+				}
+
+				if ((targetTileType.Walls ^ (WallTypes.Top)) == 0) {
+					texture = wallsT;
+				}
+				if ((targetTileType.Walls ^ (WallTypes.Bottom)) == 0) {
+					texture = wallsB;
+				}
+				if ((targetTileType.Walls ^ (WallTypes.Right)) == 0) {
+					texture = wallsR;
+				}
+				if ((targetTileType.Walls ^ (WallTypes.Left)) == 0) {
+					texture = wallsL;
+				}
+
+				if ((targetTileType.Walls ^ (WallTypes.None)) == 0) {
+					texture = noWalls;
+				}
+
+				if ((targetTileType.Walls ^ (WallTypes.Left|WallTypes.Bottom)) == 0) {
+					texture = wallsBL;
+				}
+
+				if ((targetTileType.Walls ^ (WallTypes.Top|WallTypes.Left)) == 0) {
+					texture = wallsTL;
+				}
+
+				if ((targetTileType.Walls ^ (WallTypes.Right|WallTypes.Bottom)) == 0) {
+					texture = wallsBR;
+				}
+
+				if ((targetTileType.Walls ^ (WallTypes.Top|WallTypes.Right)) == 0) {
+					texture = wallsTR;
+				}
+
+				//
+
+				if ((targetTileType.Walls ^ (WallTypes.Left|WallTypes.Top|WallTypes.Bottom)) == 0) {
+					texture = wallsLTB;
+				}
+
+				if ((targetTileType.Walls ^ (WallTypes.Right|WallTypes.Top|WallTypes.Bottom)) == 0) {
+					texture = wallsRTB;
+				}
+
+				if ((targetTileType.Walls ^ (WallTypes.Left|WallTypes.Right|WallTypes.Top|WallTypes.Bottom)) == 0) {
+					texture = wallsLTRB;
+				}
+
+				//
+
+				if ((targetTileType.Walls ^ (WallTypes.Right|WallTypes.Left|WallTypes.Bottom)) == 0) {
+					texture = wallsLBR;
+				}
+
+				if ((targetTileType.Walls ^ (WallTypes.Left|WallTypes.Right|WallTypes.Top|WallTypes.Left)) == 0) {
+					texture = wallsLTR;
+				}
+
+
+				return texture;
+			}
+
+			Color SelectRoomColor (int l, int c)
+			{
+				var targetTileType = level.Tiles [c + l * mapWidth];
+
+				if (!targetTileType.MainPath)
+					return Color.LightYellow;
+				else if (targetTileType.Start)
+					return Color.LightGreen;
+				else if (targetTileType.Finish)
+					return Color.LightSalmon;
+				else
+					return Color.LightCyan;
+			}
+
+			bool IsEmptyRoom (int l, int c)
+			{
+				var targetTileType = level.Tiles [c + l * mapWidth];
+				return targetTileType.Empty;
+			}
+
+			void DumpTileInfo (int l, int c)
+			{
+				var targetTileType = level.Tiles [c + l * mapWidth];
+				Console.WriteLine (targetTileType.Walls);
+			}
+		}
+
+		class BathysphereEntity
+		{
+			Texture2D bathyspehere;
+			float posx = 0;
+			float posy = 0;
+
+			public void Load(ContentManager content)
+			{
+				bathyspehere = content.Load<Texture2D> ("bathysphere-small");
+			}
+
+			public void Render (SpriteBatch spriteBatch)
+			{
+				if (bathyspehere != null)
+					spriteBatch.Draw( bathyspehere, new Rectangle((int)posx-5, (int)posy-5, 10, 10), null, Color.White);
+			}
+
+			public void Update (GameTime gameTime)
+			{
+			}
+
+			public void SetPosition (float posx, float posy)
+			{
+				this.posx = posx;
+				this.posy = posy;
+			}
+
+			public void ApplyForce (int xForce, int yForce)
+			{
+			}
+		}
+
 		#region Fields
 
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
-		Texture2D wallsTB;
-		Texture2D wallsRL;
-		Texture2D wallsT;
-		Texture2D wallsB;
-		Texture2D wallsR;
-		Texture2D wallsL;
-		Texture2D noWalls;
-		Texture2D wallsBL;
-		Texture2D wallsTL;
-		Texture2D wallsBR;
-		Texture2D wallsTR;
-		Texture2D wallsLTB;
-		Texture2D wallsRTB;
-		Texture2D wallsLTRB;
 
-		Texture2D wallsLBR;
-
-		Texture2D wallsLTR;
-
-		Texture2D bathyspehere;
+		LevelEntity levelEntity;
+		BathysphereEntity bathysphereEntity;
 
 		Keys[] oldKeyDowns;
 
@@ -64,7 +256,7 @@ namespace PosEd
 
 		#region Initialization
 
-		LevelDescription level;
+		LevelDescription levelDescription;
 
 		const float ROOM_SCALE = 2.0f;
 
@@ -77,13 +269,14 @@ namespace PosEd
 		const int WORLD_WIDTH = MAP_WIDTH * TILE_WIDTH;
 		const int WORLD_HEIGHT = MAP_HEIGHT * TILE_HEIGHT;
 
+		const int SCREEN_WIDTH = 6 * 160;
+		const int SCREEN_HEIGHT = 6 * 90;
+
 		bool slow = false;
 		int slowCount = 0;
 		int delta = 100;
 
 		bool game = false;
-		float posx = 0;
-		float posy = 0;
 
 		public Game1 ()
 		{
@@ -93,17 +286,20 @@ namespace PosEd
 			Content.RootDirectory = "Content";
 
 			graphics.IsFullScreen = false;
-			graphics.PreferredBackBufferWidth = WORLD_WIDTH;
-			graphics.PreferredBackBufferHeight = WORLD_HEIGHT;
+			graphics.PreferredBackBufferWidth = SCREEN_WIDTH;
+			graphics.PreferredBackBufferHeight = SCREEN_HEIGHT;
 
 			GenerateLevel ();
 		}
 
 		void GenerateLevel ()
 		{
-			slow = false;
-			level = LevelGenerator.Generate(MAP_WIDTH, MAP_HEIGHT);
 
+			levelDescription = LevelGenerator.Generate(MAP_WIDTH, MAP_HEIGHT);
+			levelEntity = new LevelEntity(levelDescription, MAP_WIDTH, MAP_HEIGHT);
+			bathysphereEntity = new BathysphereEntity ();
+
+			slow = false;
 			SetStartXY ();
 		}
 
@@ -113,7 +309,7 @@ namespace PosEd
 			if (slow)
 				slowCount = 0;
 			else
-				level = new LevelDescription(MAP_WIDTH, MAP_HEIGHT, LevelGenerator.Context.LastSnapshot);
+				levelDescription = new LevelDescription(MAP_WIDTH, MAP_HEIGHT, LevelGenerator.Context.LastSnapshot);
 		}
 
 		void GameMode()
@@ -124,14 +320,14 @@ namespace PosEd
 
 		void SetStartXY ()
 		{
-			var startIndex = level.Tiles.FindIndex (tile => tile.Start);
-			var c = (startIndex % level.Width) + 0.5f;
-			var l = (startIndex / level.Width) + 0.5f;
+			var startIndex = levelDescription.Tiles.FindIndex (tile => tile.Start);
+			var c = (startIndex % levelDescription.Width) + 0.5f;
+			var l = (startIndex / levelDescription.Width) + 0.5f;
 
-			posx = (c * (float)TILE_WIDTH);
-			posy = (l * (float)TILE_HEIGHT);
+			float posx = (c * (float)TILE_WIDTH);
+			float posy = (l * (float)TILE_HEIGHT);
 
-			Console.WriteLine (String.Format("{0} {1} {2} {3}", l, c, posx, posy));
+			bathysphereEntity.SetPosition (posx, posy);
 		}			
 
 		/// <summary>
@@ -151,29 +347,8 @@ namespace PosEd
 		{
 			// Create a new SpriteBatch, which can be use to draw textures.
 			spriteBatch = new SpriteBatch (graphics.GraphicsDevice);
-
-			wallsTB = Content.Load<Texture2D> ("square-tb");
-			wallsRL = Content.Load<Texture2D> ("square-rl");
-			wallsT = Content.Load<Texture2D> ("square-t");
-			wallsB = Content.Load<Texture2D> ("square-b");
-			wallsR = Content.Load<Texture2D> ("square-r");
-			wallsL = Content.Load<Texture2D> ("square-l");
-
-			noWalls = Content.Load<Texture2D> ("square");
-
-			wallsBL = Content.Load<Texture2D> ("square-bl");
-			wallsTL = Content.Load<Texture2D> ("square-lt");
-			wallsBR = Content.Load<Texture2D> ("square-rb");
-			wallsTR = Content.Load<Texture2D> ("square-tr");
-
-			wallsLTB = Content.Load<Texture2D> ("square-ltb");
-			wallsRTB = Content.Load<Texture2D> ("square-rtb");
-			wallsLTRB = Content.Load<Texture2D> ("square-ltrb");
-
-			wallsLBR = Content.Load<Texture2D> ("square-lbr");
-			wallsLTR = Content.Load<Texture2D> ("square-ltr");
-
-			bathyspehere = Content.Load<Texture2D> ("bathysphere-small");
+			levelEntity.Load(Content);
+			bathysphereEntity.Load (Content);
 		}
 
 		#endregion
@@ -213,11 +388,11 @@ namespace PosEd
 			}
 
 			if (keyStillDowns.Contains (Keys.Left)) {
-				posx -= ((20 * gameTime.ElapsedGameTime.Milliseconds) / 1000.0f);
-				Console.WriteLine (String.Format("{0} {1}", posx, posy));
+				bathysphereEntity.ApplyForce (-2, 0);
 			}
 
 			if (keyStillDowns.Contains (Keys.Right)) {
+				bathysphereEntity.ApplyForce (2, 0);
 			}
 
 			if (keyStillDowns.Contains (Keys.Up)) {
@@ -244,12 +419,12 @@ namespace PosEd
 				}
 			}
 
-			level = new LevelDescription(MAP_WIDTH, MAP_HEIGHT, LevelGenerator.Context.Snapshots[slowCount]);
+			levelDescription = new LevelDescription(MAP_WIDTH, MAP_HEIGHT, LevelGenerator.Context.Snapshots[slowCount]);
 		}
 
 		void UpdateGame(GameTime gameTime)
 		{
-
+			bathysphereEntity.Update (gameTime);
 		}
 
 		/// <summary>
@@ -280,123 +455,10 @@ namespace PosEd
 
 		void RenderMap ()
 		{
-			for (int l = 0; l < level.Height; l++) {
-				for (int c = 0; c < level.Width; c++) {
-					var rectangle = new Rectangle (c * TILE_WIDTH, l * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
-					var texture = SelectRoomTexture (l, c);
-					var color = SelectRoomColor (l, c);
-					if (!IsEmptyRoom (l, c)) {
-						if (texture != null) {
-							spriteBatch.Draw (texture, rectangle, null, color);
-						}
-						else {
-							DumpTileInfo (l, c);
-						}
-					}
-				}
-			}
-
-			spriteBatch.Draw( bathyspehere, new Rectangle((int)posx-5, (int)posy-5, 10, 10), null, Color.White);
+			levelEntity.Render (levelDescription, spriteBatch);
+			bathysphereEntity.Render (spriteBatch);
 		}
 			
-		Texture2D SelectRoomTexture (int l, int c)
-		{
-			Texture2D texture = null;
-			var targetTileType = level.Tiles [c + l * MAP_WIDTH];
-
-			if ((targetTileType.Walls ^ (WallTypes.Top|WallTypes.Bottom)) == 0) {
-				texture = wallsTB;
-			}
-			if ((targetTileType.Walls ^ (WallTypes.Right|WallTypes.Left)) == 0) {
-				texture = wallsRL;
-			}
-
-			if ((targetTileType.Walls ^ (WallTypes.Top)) == 0) {
-				texture = wallsT;
-			}
-			if ((targetTileType.Walls ^ (WallTypes.Bottom)) == 0) {
-				texture = wallsB;
-			}
-			if ((targetTileType.Walls ^ (WallTypes.Right)) == 0) {
-				texture = wallsR;
-			}
-			if ((targetTileType.Walls ^ (WallTypes.Left)) == 0) {
-				texture = wallsL;
-			}
-
-			if ((targetTileType.Walls ^ (WallTypes.None)) == 0) {
-				texture = noWalls;
-			}
-
-			if ((targetTileType.Walls ^ (WallTypes.Left|WallTypes.Bottom)) == 0) {
-				texture = wallsBL;
-			}
-
-			if ((targetTileType.Walls ^ (WallTypes.Top|WallTypes.Left)) == 0) {
-				texture = wallsTL;
-			}
-
-			if ((targetTileType.Walls ^ (WallTypes.Right|WallTypes.Bottom)) == 0) {
-				texture = wallsBR;
-			}
-
-			if ((targetTileType.Walls ^ (WallTypes.Top|WallTypes.Right)) == 0) {
-				texture = wallsTR;
-			}
-
-			//
-
-			if ((targetTileType.Walls ^ (WallTypes.Left|WallTypes.Top|WallTypes.Bottom)) == 0) {
-				texture = wallsLTB;
-			}
-
-			if ((targetTileType.Walls ^ (WallTypes.Right|WallTypes.Top|WallTypes.Bottom)) == 0) {
-				texture = wallsRTB;
-			}
-
-			if ((targetTileType.Walls ^ (WallTypes.Left|WallTypes.Right|WallTypes.Top|WallTypes.Bottom)) == 0) {
-				texture = wallsLTRB;
-			}
-
-			//
-
-			if ((targetTileType.Walls ^ (WallTypes.Right|WallTypes.Left|WallTypes.Bottom)) == 0) {
-				texture = wallsLBR;
-			}
-
-			if ((targetTileType.Walls ^ (WallTypes.Left|WallTypes.Right|WallTypes.Top|WallTypes.Left)) == 0) {
-				texture = wallsLTR;
-			}
-
-
-			return texture;
-		}
-
-		Color SelectRoomColor (int l, int c)
-		{
-			var targetTileType = level.Tiles [c + l * MAP_WIDTH];
-
-			if (!targetTileType.MainPath)
-				return Color.LightYellow;
-			else if (targetTileType.Start)
-				return Color.LightGreen;
-			else if (targetTileType.Finish)
-				return Color.LightSalmon;
-			else
-				return Color.LightCyan;
-		}
-
-		bool IsEmptyRoom (int l, int c)
-		{
-			var targetTileType = level.Tiles [c + l * MAP_WIDTH];
-			return targetTileType.Empty;
-		}
-
-		void DumpTileInfo (int l, int c)
-		{
-			var targetTileType = level.Tiles [c + l * MAP_WIDTH];
-			Console.WriteLine (targetTileType.Walls);
-		}
 		#endregion
 	}
 }
